8693bafc74f6a5c6136a46463c730ee55c07ef76
imjacky <imjackyrm@gmail.com>
2022-07-18 09:33:20 +0800

    income component update

--- src/app/financial/income/dialog/record/record.component.ts
+++ src/app/financial/income/dialog/record/record.component.ts
@@ -8,6 +8,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
   templateUrl: './record.component.html',
   styleUrls: ['./record.component.scss'],
 })
+
 export class RecordComponent implements OnInit {
   categorys = []
   actionBtn = 'Save'
@@ -24,37 +25,45 @@ export class RecordComponent implements OnInit {
     this.recordForm = this.formBuilder.group({
       date: ['', Validators.required],
       month: [''],
-      category: ['', Validators.required],
+      year: [''],
+      day: [''],
+      category_id: ['', Validators.required],
       amount: ['', [Validators.required, Validators.pattern(/^[0-9]\d*$/)]],
     })
     this.getCategory()
-
-    if (this.editData) {
-      this.actionBtn = 'Update'
-      this.recordForm.controls['date'].setValue(this.editData.date)
-      this.recordForm.controls['month'].setValue(this.editData.month)
-      this.recordForm.controls['category'].setValue(this.editData.category)
-      this.recordForm.controls['amount'].setValue(this.editData.amount)
-    }
   }
 
   getCategory() {
     this.api.getIncomeCategory().subscribe({
       next: (res) => {
-        res.forEach((element) => {
-          this.categorys.push(element['category'])
-        })
+        this.categorys = res
+        this.edit_Data()
       },
       error() {
         alert('Record err')
       },
     })
   }
+  edit_Data() {
+    if (this.editData) {
+      this.actionBtn = 'Update'
+      this.recordForm.controls['date'].setValue(this.editData.date)
+      this.recordForm.controls['month'].setValue(this.editData.month)
+      this.recordForm.controls['year'].setValue(this.editData.year)
+      this.recordForm.controls['day'].setValue(this.editData.day)
+      this.categorys.forEach((element) => {
+        if (this.editData.category == element.category) {
+          this.recordForm.controls['category_id'].setValue('' + element.id)
+        }
+      })
+      this.recordForm.controls['amount'].setValue(this.editData.amount)
+    }
+  }
 
   addRecord() {
     if (!this.editData) {
       if (this.recordForm.valid) {
-        this.getMonth()
+        this.getDate()
         this.api.postIncomeRecord(this.recordForm.value).subscribe({
           next: (res) => {
             alert('Record added')
@@ -72,7 +81,7 @@ export class RecordComponent implements OnInit {
   }
 
   updateRecord() {
-    this.getMonth()
+    this.getDate()
     this.api
       .putIncomeRecord(this.recordForm.value, this.editData.id)
       .subscribe({
@@ -87,49 +96,65 @@ export class RecordComponent implements OnInit {
       })
   }
 
-  getMonth() {
-    let date = '' + this.recordForm.get('date').value
+  // 0    m1  d2  y3
+  // Mon Jul 18 2022 00:00:00 GMT+0800 (台北標準時間)
+  getDate() {
+    let date = this.recordForm.get('date').value.toString()
+    date = new Date(date)
+    console.log('getDate : ' + date)
+    date = date.toString()
     let fullDate = date.split(' ')
+    console.log("1 --")
     let month = fullDate[1]
+    console.log(`month : ${month}`)
+    console.log(`this.recordForm.get('month')`)
+    console.log(this.recordForm.get('month').value)
     switch (month) {
       case 'Jan':
-        this.recordForm.get('month').setValue(1)
+        this.recordForm.get('month').setValue("01")
         break
       case 'Feb':
-        this.recordForm.get('month').setValue(2)
+        this.recordForm.get('month').setValue("02")
         break
       case 'Mar':
-        this.recordForm.get('month').setValue(3)
+        this.recordForm.get('month').setValue("03")
         break
       case 'Apr':
-        this.recordForm.get('month').setValue(4)
+        this.recordForm.get('month').setValue("04")
         break
       case 'May':
-        this.recordForm.get('month').setValue(5)
+        this.recordForm.get('month').setValue("05")
         break
       case 'Jun':
-        this.recordForm.get('month').setValue(6)
+        this.recordForm.get('month').setValue("06")
         break
       case 'Jul':
-        this.recordForm.get('month').setValue(7)
+        this.recordForm.get('month').setValue("07")
         break
       case 'Aug':
-        this.recordForm.get('month').setValue(8)
+        this.recordForm.get('month').setValue("08")
         break
       case 'Sep':
-        this.recordForm.get('month').setValue(9)
+        this.recordForm.get('month').setValue("09")
         break
       case 'Oct':
-        this.recordForm.get('month').setValue(10)
+        this.recordForm.get('month').setValue("10")
         break
       case 'Nov':
-        this.recordForm.get('month').setValue(11)
+        this.recordForm.get('month').setValue("11")
         break
       case 'Dec':
-        this.recordForm.get('month').setValue(12)
+        this.recordForm.get('month').setValue("12")
         break
       default:
         console.log(`Sorry, ${month}`)
     }
+    console.log(`year : `)
+    console.log(`${fullDate[3]}`)
+    this.recordForm.get('year').setValue(fullDate[3])
+    console.log(`day : `)
+    console.log(`${fullDate[2]}`)
+    this.recordForm.get('day').setValue(fullDate[2])
+    console.log("end")
   }
 }
--- src/app/financial/income/income.component.ts
+++ src/app/financial/income/income.component.ts
@@ -1,13 +1,13 @@
+import { element } from 'protractor'
 import { ApiService } from './../../services/api.service'
 import { RecordComponent } from './dialog/record/record.component'
 import { CategoryComponent } from './dialog/category/category.component'
 
-import { Component, OnInit, ViewChild } from '@angular/core'
+import { Component, Input, OnInit, ViewChild } from '@angular/core'
 import { MatDialog } from '@angular/material/dialog'
-
-import { MatPaginator } from '@angular/material/paginator'
-import { MatSort } from '@angular/material/sort'
-import { MatTableDataSource } from '@angular/material/table'
+declare var $: any
+import * as Chartist from 'chartist'
+import { fileURLToPath } from 'url'
 
 @Component({
   selector: 'app-income',
@@ -15,118 +15,475 @@ import { MatTableDataSource } from '@angular/material/table'
   styleUrls: ['./income.component.scss'],
 })
 export class IncomeComponent implements OnInit {
-  displayedColumns: string[] = ['date', 'category', 'amount']
-  dataSource!: MatTableDataSource<any>
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator
-  @ViewChild(MatSort) sort!: MatSort
-
   constructor(public dialog: MatDialog, private api: ApiService) {}
 
   icHistorys = []
   icCategorys = []
+  new_icCategorys_list = []
+  old_icCategorys_list = []
+  selectedAmount = 0
+  maxAmount = 0
+  monthly = 0
+  yearly = 0
+  totalIncome = 0
+  selectedYear = 0
+  selectedMonth = 0
+  isResult = []
+  years = []
+  categorys = []
+  months = [
+    { show: '1', value: '01' },
+    { show: '2', value: '02' },
+    { show: '3', value: '03' },
+    { show: '4', value: '04' },
+    { show: '5', value: '05' },
+    { show: '6', value: '06' },
+    { show: '7', value: '07' },
+    { show: '8', value: '08' },
+    { show: '9', value: '09' },
+    { show: '10', value: '10' },
+    { show: '11', value: '11' },
+    { show: '12', value: '12' },
+  ]
+  yearV = null
+  monthV = null
+  categoryV = null
 
   ngOnInit(): void {
-    this.getIncome()
-    this.getCategory()
+    this.onload()
   }
 
+  // Record
   addRecord() {
     this.dialog
-      .open(RecordComponent, {
-        width: '100%',
-      })
+      .open(RecordComponent, { width: '100%' })
       .afterClosed()
       .subscribe((val) => {
         if (val === 'save') {
-          this.getIncome()
-          this.getCategory()
+          this.onload()
         }
       })
   }
   editRecord(icHistory: any) {
     this.dialog
-      .open(RecordComponent, {
-        width: '100%',
-        data: icHistory,
-      })
+      .open(RecordComponent, { width: '100%', data: icHistory })
       .afterClosed()
       .subscribe((val) => {
         if (val === 'update') {
-          this.getIncome()
-          this.getCategory()
+          this.onload()
         }
       })
   }
+  deleteRecord(id: number) {
+    // console.log(id)
+    this.api.deleteIncomeRecord(id).subscribe({
+      next: (res) => {
+        alert(`Record delete`)
+        this.onload()
+      },
+      error() {
+        alert('Record err')
+      },
+    })
+  }
 
+  // Category
   addCategory() {
+    console.log(`addCategory`)
     this.dialog
       .open(CategoryComponent, {})
       .afterClosed()
       .subscribe((val) => {
         if (val === 'save') {
-          this.getIncome()
-          this.getCategory()
+          this.onload()
+        }
+      })
+  }
+  editCategory(icCategory: any) {
+    console.log(`editCategory`)
+    this.dialog
+      .open(CategoryComponent, { width: '100%', data: icCategory })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'update') {
+          this.onload()
         }
       })
   }
 
-  getIncome() {
-    this.api.getIncomeRecord().subscribe({
-      next: (res) => {
-        this.icHistorys = res
-        this.dataSource = new MatTableDataSource(res)
-        this.dataSource.paginator = this.paginator
-        this.dataSource.sort = this.sort
-      },
-      error() {
-        alert('Record err')
-      },
-    })
+  // Button function
+  onChangeSelet(attr: any, value: any) {
+    console.log(`onChangeSelet + ${value}`)
+    if (attr == 'year') {
+      this.yearV = value
+    }
+    if (attr == 'month') {
+      this.monthV = value
+    }
+    if (attr == 'category_id') {
+      this.categoryV = value
+    }
+  }
+  search() {
+    console.log(`search`)
+    this.selectedAmount = 0
+    this.monthly = 0
+    this.yearly = 0
+    if (this.yearV != null) {
+      if (this.monthV != null && this.categoryV != null) {
+        this.api_ymc3(
+          'year',
+          this.yearV,
+          'month',
+          this.monthV,
+          'category_id',
+          this.categoryV,
+        )
+      } else if (this.monthV != null) {
+        this.api_ymc2('year', this.yearV, 'month', this.monthV)
+      } else if (this.categoryV != null) {
+        this.api_ymc2('year', this.yearV, 'category_id', this.categoryV)
+      } else {
+        this.api_ymc1('year', this.yearV)
+      }
+    } else if (this.monthV != null) {
+      if (this.categoryV != null) {
+        this.api_ymc2('month', this.monthV, 'category_id', this.categoryV)
+      } else {
+        this.api_ymc1('month', this.monthV)
+      }
+    } else if (
+      this.categoryV != null &&
+      this.monthV == null &&
+      this.yearV == null
+    ) {
+      this.api_ymc1('category_id', this.categoryV)
+    } else {
+      this.showNotification('Please select at least one of selection')
+    }
+  }
+  refresh() {
+    window.location.reload()
   }
 
+  // ngOnInit()
+  onload() {
+    this.getCategory()
+  }
   getCategory() {
     this.api.getIncomeCategory().subscribe({
       next: async (res) => {
         this.icCategorys = res
+        this.getIncome()
+      },
+      error() {
+        alert('Record err')
+      },
+    })
+  }
+  getIncome() {
+    this.api.getIncomeRecord().subscribe({
+      next: (res) => {
         res.forEach((element) => {
-          this.findExpectAmount(element.category, element)
+          this.icCategorys.forEach((c) => {
+            if (c.id == element.category_id) {
+              element['category'] = c.category
+            }
+          })
+        })
+        this.icHistorys = res
+        this.old_icCategorys_list = this.icCategorys
+        this.setSelectYear()
+        this.setChart()
+      },
+      error() {
+        alert('Record err')
+      },
+    })
+  }
+  // extend
+  api_ymc1(attr1: any, v1: any) {
+    console.log('api_ymc1')
+    let new_icCategorys_id = null
+    this.api.getIncomeRecord_ync1(attr1, v1).subscribe({
+      next: (res) => {
+        res.forEach((element) => {
+          this.icCategorys.forEach((c) => {
+            if (c.id == element.category_id) {
+              element['category'] = c.category
+              new_icCategorys_id = c.id
+            }
+          })
+        })
+        this.icHistorys = res
+        if (attr1 == 'category_id') {
+          this.getSelectedCategoryList(new_icCategorys_id)
+        }
+        this.getSelectedTotalAmount(res)
+        this.setChart()
+      },
+      error() {
+        alert('Record err')
+      },
+    })
+  }
+  api_ymc2(attr1: any, v1: any, attr2: any, v2: any) {
+    console.log('api_ymc2')
+    let new_icCategorys_id = null
+    this.api.getIncomeRecord_ync2(attr1, v1, attr2, v2).subscribe({
+      next: (res) => {
+        res.forEach((element) => {
+          this.icCategorys.forEach((c) => {
+            if (c.id == element.category_id) {
+              element['category'] = c.category
+              new_icCategorys_id = c.id
+            }
+          })
+        })
+        if (attr1 == 'category_id' || attr2 == 'category_id') {
+          this.getSelectedCategoryList(new_icCategorys_id)
+        }
+        this.icHistorys = res
+        this.getSelectedTotalAmount(res)
+        this.setChart()
+      },
+      error() {
+        alert('Record err')
+      },
+    })
+  }
+  api_ymc3(attr1: any, v1: any, attr2: any, v2: any, attr3: any, v3: any) {
+    console.log('api_ymc3')
+    let new_icCategorys_id = null
+    this.api.getIncomeRecord_ync3(v1, v2, v3).subscribe({
+      next: (res) => {
+        res.forEach((element) => {
+          this.icCategorys.forEach((c) => {
+            if (c.id == element.category_id) {
+              element['category'] = c.category
+              new_icCategorys_id = c.id
+            }
+          })
+        })
+        this.icHistorys = res
+        this.getSelectedCategoryList(new_icCategorys_id)
+        if (attr1 == 'year') {
+          this.getSelectedYearAmount(res, attr1, v1)
+        }
+        if (attr2 == 'month') {
+          this.getSelectedMonthAmount(res, attr1, v1, attr2, v2)
+        }
+        this.getSelectedTotalAmount(res)
+        this.setChart()
+      },
+      error() {
+        alert('Record err')
+      },
+    })
+  }
+  getSelectedCategoryList(new_icCategorys_id: any) {
+    console.log(`getSelectedCategoryList`)
+    this.new_icCategorys_list = []
+    this.icCategorys.forEach((c) => {
+      if (c.id == new_icCategorys_id) {
+        this.new_icCategorys_list.push(c)
+      }
+    })
+    this.old_icCategorys_list = this.icCategorys
+    this.icCategorys = this.new_icCategorys_list
+  }
+  // selected year total
+  getSelectedYearAmount(res: any, attr1: any, v1: any) {
+    this.selectedYear = 0
+    this.api.getIncomeRecord_ync1(attr1, v1).subscribe({
+      next: (res) => {
+        res.forEach((element) => {
+          this.selectedYear += parseInt(element.amount)
+        })
+        console.log(`selectedYear/${this.selectedYear}`)
+      },
+      error() {},
+    })
+  }
+  // selected month total
+  getSelectedMonthAmount(res: any, attr1: any, v1: any, attr2: any, v2: any) {
+    this.selectedMonth = 0
+    this.api.getIncomeRecord_ync2(attr1, v1, attr2, v2).subscribe({
+      next: (res) => {
+        res.forEach((element) => {
+          this.selectedMonth += parseInt(element.amount)
         })
       },
-      error() {
-        alert('Record err')
-      },
+      error() {},
     })
   }
+  // selected total
+  getSelectedTotalAmount(res: any) {
+    console.log(`getSelectedTotalAmount`)
+    res.forEach((element: any) => {
+      this.selectedAmount += parseInt(element.amount)
+    })
+  }
+  setSelectYear() {
+    console.log(`setSelectYear`)
+    let yearSort = []
+    this.icHistorys.forEach((element) => {
+      yearSort.push(element.year)
+    })
+    yearSort.sort((a, b) => a > b ? -1 : 0)
+    this.years = yearSort.filter((element, index) => {
+      return yearSort.indexOf(element) === index
+    })
+  }
+  showNotification(message: any, from: any = 'top', align: any = 'right') {
+    $.notify(
+      {
+        icon: 'notifications',
+        message: message,
+      },
+      {
+        type: 'danger',
+        timer: 4000,
+        placement: {
+          from: from,
+          align: align,
+        },
+        template:
+          '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+          '<button mat-button type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss"><i class="material-icons">close</i></button>' +
+          '<i class="material-icons" data-notify="icon">notifications</i>' +
+          '<span data-notify="title">{1}</span>' +
+          '<span data-notify="message">{2}</span>' +
+          '<div class="progress" data-notify="progressbar">' +
+          '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+          '</div>' +
+          '<a href="{3}" target="{4}" data-notify="url"></a>' +
+          '</div>',
+      },
+    )
+  }
+  // extend - Draw Chart
+  setChart() {
+    console.log('setChart')
+    let day = []
+    let amount = []
+    this.maxAmount = 0
 
-  findExpectAmount(resCategory: any, res: any) {
-    var totalAmount = 0
-    this.api.getIncomeRecord_category(resCategory).subscribe({
-      next: async (res_record) => {
-        if (res_record.length > 0) {
-          res_record.forEach((element) => {
-            totalAmount += parseInt(element.amount)
-          })
-          res.amount = totalAmount.toString()
+    // sorting
+    console.log(`sorting`)
+    this.icHistorys.sort(function (a, b) {
+      var keyA = new Date(a.date),
+        keyB = new Date(b.date)
+      // Compare the 2 dates
+      if (keyA < keyB) return 1
+      if (keyA > keyB) return -1
+      return 0
+    })
+
+    // set label day
+    console.log(`setLabelDay`)
+    this.icHistorys.forEach((element) => {
+      let printDate = `${element.year}/${element.month}/${element.day}`
+      day.push(printDate)
+    })
+    day.sort()
+    day = day.filter((element, index) => {
+      return day.indexOf(element) === index
+    })
+
+    // set line amount
+    console.log(`setLineAmount`)
+    this.icCategorys.forEach((element) => {
+      let amountPush = []
+      let categorySum = 0
+      let id = '' + element.id
+      day.forEach((dayElement) => {
+        let filterResult = this.icHistorys.filter(
+          (element) =>
+            `${element.year}/${element.month}/${element.day}` == dayElement &&
+            element.category_id == id,
+        )
+        let sum: number = 0
+        filterResult.forEach((fR) => {
+          sum += parseInt(fR.amount)
+        })
+        // set high of Chart
+        if (sum > this.maxAmount) {
+          this.maxAmount = sum + 25
         }
-      },
-      error() {
-        alert('Record err')
-      },
-    })
-  }
+        amountPush.push(sum)
+        categorySum += sum
+      })
+      element.amount = categorySum.toString()
 
-  deleteRecord(id: number) {
-    console.log(id)
-    this.api.deleteIncomeRecord(id).subscribe({
-      next: (res) => {
-        alert(`Record delete`)
-        this.getIncome()
-        this.getCategory()
-      },
-      error() {
-        alert('Record err')
-      },
+      console.log(`categorySum`)
+      console.log(categorySum.toString())
+      amount.push(amountPush)
     })
+
+    const data_lineChartIncome: any = {
+      labels: day,
+      series: amount,
+    }
+
+    const options_lineChartIncome: any = {
+      lineSmooth: Chartist.Interpolation.cardinal({
+        tension: 0,
+      }),
+      low: 0,
+      height: 300,
+      high: this.maxAmount,
+      chartPadding: { top: 0, right: 0, bottom: 0, left: 0 },
+    }
+
+    var lineChartIncome = new Chartist.Line(
+      '#lineChartIncome', // class
+      data_lineChartIncome,
+      options_lineChartIncome,
+    )
+
+    this.startAnimationForLineChart(lineChartIncome)
+
+    // reset for select option
+    this.icCategorys = this.old_icCategorys_list
+    if (this.icHistorys.length === 0) {
+      this.showNotification('No date found. Please try again.')
+    }
+  }
+  startAnimationForLineChart(chart) {
+    let seq: any, delays: any, durations: any
+    seq = 0
+    delays = 80
+    durations = 500
+
+    chart.on('draw', function (data) {
+      if (data.type === 'line' || data.type === 'area') {
+        data.element.animate({
+          d: {
+            begin: 600,
+            dur: 700,
+            from: data.path
+              .clone()
+              .scale(1, 0)
+              .translate(0, data.chartRect.height())
+              .stringify(),
+            to: data.path.clone().stringify(),
+            easing: Chartist.Svg.Easing.easeOutQuint,
+          },
+        })
+      } else if (data.type === 'point') {
+        seq++
+        data.element.animate({
+          opacity: {
+            begin: seq * delays,
+            dur: durations,
+            from: 0,
+            to: 1,
+            easing: 'ease',
+          },
+        })
+      }
+    })
+    seq = 0
   }
 }
