d412e78f68608676e4d913be0f1b5bab4d2bff88
imjacky <imjackyrm@gmail.com>
2022-07-25 09:25:25 +0800

    report component update

--- src/app/plan/ieplan/dialog/report/report.component.ts
+++ src/app/plan/ieplan/dialog/report/report.component.ts
@@ -1,15 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import { ApiService } from "./../../../../services/api.service"
 
 @Component({
-  selector: 'app-report',
-  templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+  selector: "app-report",
+  templateUrl: "./report.component.html",
+  styleUrls: ["./report.component.scss"],
 })
 export class ReportComponent implements OnInit {
+  constructor(private api: ApiService) {}
+  iCategorys = []
+  eCategorys = []
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getiCategory() {
+    console.log(`--- getiCategory`)
+    let year = new Date().getFullYear()
+    let iCat_DailyAmount = []
+    this.api.getIncomeCategory().subscribe({
+      next: res => {
+        res.forEach(element => {
+          let totalAmount = 0
+          element.amount = totalAmount
+          this.api.getIncomeRecord_ync2("year", year, "category_id", element.id).subscribe({
+            next: res => {
+              if (res.length > 0) {
+                res.forEach(element => {
+                  totalAmount += parseInt(element.amount)
+                })
+              }
+              element.amount = totalAmount
+            },
+            error() {
+              alert("Record err")
+            },
+          })
+        })
+        this.iCategorys = res
+        this.api.getIncomeRecord_ync1("year", year).subscribe({
+          next: res => {},
+          error() {
+            alert("Record err")
+          },
+        })
+
+        this.geteCategory()
+      },
+      error() {
+        alert("Record err")
+      },
+    })
   }
-
+  geteCategory() {
+    console.log(`--- geteCategory`)
+    let year = new Date().getFullYear()
+    this.api.getExpenditureCategory().subscribe({
+      next: res => {
+        res.forEach(element => {
+          let totalAmount = 0
+          element.amount = totalAmount
+          this.api.getExpenditureRecord_ync2("year", year, "category_id", element.id).subscribe({
+            next: res => {
+              if (res.length > 0) {
+                res.forEach(element => {
+                  totalAmount += parseInt(element.amount)
+                })
+              }
+              element.amount = totalAmount
+            },
+            error() {
+              alert("Record err")
+            },
+          })
+        })
+        this.eCategorys = res
+        this.api.getExpenditureRecord_ync1("year", year).subscribe({
+          next: res => {},
+          error() {
+            alert("Record err")
+          },
+        })
+      },
+      error() {
+        alert("Record err")
+      },
+    })
+  }
 }
